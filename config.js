70838a00627dbb12e9e37d371971f3b3e23360c6
Yifei Teng <yifei@skycatch.com>
2015-07-09 16:18:30 -0700

    Update dependencies

--- config.js
+++ config.js
@@ -16,13 +16,13 @@ System.config({
 System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.4.2",
-    "babel": "npm:babel-core@5.6.15",
-    "babel-runtime": "npm:babel-runtime@5.6.15",
+    "babel": "npm:babel-core@5.6.17",
+    "babel-runtime": "npm:babel-runtime@5.6.17",
     "core-js": "npm:core-js@0.9.18",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:babel-runtime@5.6.15": {
+    "npm:babel-runtime@5.6.17": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.18": {
